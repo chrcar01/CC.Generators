824875cf8f7b95a47289e084db17db49cfcfe874
Chris Carter <chris.carter@nutrien.com>
2025-05-22 05:42:01 -0700

    added partial mock generator

---
+++ CC.Generators/TestClassToGenerate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CC.Generators;
+
+public record TestClassToGenerate(string Name, string Accessibility, ITypeSymbol TargetType)
+
+{
+    public readonly string Name = Name;
+
+    public readonly ITypeSymbol TargetType = TargetType;
+
+    public readonly string Accessibility = Accessibility;
+
+    public INamedTypeSymbol? ResultType { get; set; }
+    public IEnumerable<string>? Usings { get; set; }
+    public string? Namespace { get; set; }
+}
